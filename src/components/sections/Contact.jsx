58dec8cc06a69600326c1d977ad001bf74769ea1
Van Chuong <vanchuong@tuoitre.com.vn>
2025-04-08 16:13:51 +0700

    push

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -64,7 +64,7 @@ const openNotification=()=>{
             {" "}
             Get In Touch
           </h2>
-          <form className="space-y-6" onSubmit={handleSubmit}>
+          <form className="space-y-6 cursor-pointer" onSubmit={handleSubmit}>
             <div className="relative">
               <input
                 type="text"
@@ -112,7 +112,7 @@ const openNotification=()=>{
 
             <button
               type="submit"
-              className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+              className=" cursor-pointer w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
             >
               Send Message
             </button>
